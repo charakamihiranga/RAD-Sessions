8d634f1a466126d10fa2f77b581724be3a2fe4dd
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-22 10:08:27 +0530

    ts tupples

---
+++ Day-02/tuples.js
@@ -0,0 +1,12 @@
+/**
+ * ======== TypeScript tuples ========
+ * A tuple is a TypeScript type that works like an array with some additional considerations.
+ * A tuple type is a fixed-length array where the elements have fixed types.
+ * The number of elements in the tuple is fixed, and the types of elements are known.
+ * The elements of the tuple can be of different types.
+ * The elements of the tuple can be accessed using the index.
+ * The elements of the tuple can be assigned to variables.
+ */
+var t1 = ['John', 25];
+var t2 = [21, "charaka"];
+console.log(t1);
---
+++ Day-02/tuples.ts
@@ -0,0 +1,14 @@
+/**
+ * ======== TypeScript tuples ========
+ * A tuple is a TypeScript type that works like an array with some additional considerations.
+ * A tuple type is a fixed-length array where the elements have fixed types.
+ * The number of elements in the tuple is fixed, and the types of elements are known.
+ * The elements of the tuple can be of different types.
+ * The elements of the tuple can be accessed using the index.
+ * The elements of the tuple can be assigned to variables.
+ */
+
+let t1: [string, number] = ['John', 25];
+let t2: [number, string] = [ 21 , "charaka"];
+
+console.log(t1)
