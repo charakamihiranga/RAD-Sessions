1b0a53a83fce53c78877d6c85075b0f74a8d6ff5
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-22 09:59:42 +0530

    merge ts arrays

--- Day-02/demo.js
+++ Day-02/demo.js
@@ -1,17 +1,27 @@
 // #########Typescript Variables #########
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // ======== Type Declaration =======
 var x; // type declaration for variable
 var y; // assigning never type to variable means it will never be assigned a value
 var z = 1234; // Declares a variable 'z' that can hold a value of type number, string, or boolean.
 // Initially, 'z' is assigned the number 1234.
 z = "Hello World"; // Now, 'z' is assigned a string value
-console.log(z);
+// console.log(z); 
 // ======== Typescript Arrays =======
 var ar1 = [1, 2, 3, 4, 5]; // Declares an array of numbers
 var ar2 = ["Hello", "World"]; // Declares an array of strings
 var ar3 = [1, 2, 3, "Hello", true]; // Declares an array of mixed types
 var ar4 = [];
-// let ar5 = any[]=[]; 
+// let ar4 =[1,2,3] // cannot declare empty array and assign values later
+var ar5 = []; // Declares an array of any type
 var ar6; // Declares an array of strings
 ar6 = ["Hello", "World"];
 function printarray(ar) {
@@ -20,4 +30,11 @@ function printarray(ar) {
     });
 }
 // printarray(ar2); // string [] not support
-printarray(ar1);
+// printarray(ar1);
+// merge two arrays
+var ar7 = ['a', 'b', 'c'];
+var ar8 = ['d', 'e', 'f'];
+// ar7.push(...ar8);
+// console.log(ar7);
+var ar9 = __spreadArray(__spreadArray([], ar7, true), ar8, true);
+console.log(ar9);
--- Day-02/demo.ts
+++ Day-02/demo.ts
@@ -15,8 +15,9 @@ let ar1 = [1, 2, 3, 4, 5]; // Declares an array of numbers
 let ar2 = ["Hello", "World"]; // Declares an array of strings
 let ar3 = [1, 2, 3, "Hello", true]; // Declares an array of mixed types
 
-let ar4 = [];
-// let ar5 = any[]=[]; 
+let ar4 = []; 
+// let ar4 =[1,2,3] // cannot declare empty array and assign values later
+let ar5 : any[]=[]; // Declares an array of any type
 let ar6 : Array<string>; // Declares an array of strings
 ar6 = ["Hello", "World"];
 
@@ -27,4 +28,13 @@ function printarray(ar : number[]) {
 }
 
 // printarray(ar2); // string [] not support
-printarray(ar1);
+// printarray(ar1);
+
+// merge two arrays
+let ar7 = ['a', 'b', 'c'];
+let ar8 = ['d', 'e', 'f'];
+// ar7.push(...ar8); // merge ar8 into ar7
+// console.log(ar7);
+
+let ar9 = [...ar7,...ar8]; // merge ar7 & ar8 into ar9
+console.log(ar9); 
