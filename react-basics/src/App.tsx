0c6d9dc2f0146806eb2c2c76c8de0bc3f8fa3331
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-29 16:32:08 +0530

    refactor value change function

--- react-basics/src/App.tsx
+++ react-basics/src/App.tsx
@@ -1,26 +1,18 @@
-import './App.css';
-import {ReactNode, useState} from "react";
-import { ButtonComponent } from "./component/ButtonComponent.tsx";
+import './App.css'
+import {useState} from "react";
+import {ButtonComponent} from "./component/ButtonComponent.tsx";
 
 function App() {
-    const [count, setCount] = useState(0);
 
-    const handleCLick = (value: ReactNode) => {
-        if (value === "Add") {
-            setCount(count + 1);
-        } else if ( value === "Sub") {
-            setCount(count - 1);
-        }
-    };
+    const[count, setCount] = useState(0)
 
-    return (
+    return(
         <>
             <h1>{count}</h1>
-            <br />
-            <ButtonComponent onSelect={handleCLick}>Add</ButtonComponent>
-            <ButtonComponent onSelect={handleCLick}>Sub</ButtonComponent>
+            <ButtonComponent onSelect={setCount} count={count}>+</ButtonComponent>
+            <ButtonComponent onSelect={setCount} count={count}>-</ButtonComponent>
         </>
-    );
+    )
 }
 
-export default App;
+export default App
--- react-basics/src/component/ButtonComponent.tsx
+++ react-basics/src/component/ButtonComponent.tsx
@@ -1,9 +1,20 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
-export function ButtonComponent(props: { children: ReactNode; onSelect: (value: ReactNode) => void }) {
+export function ButtonComponent(props : { children : ReactNode , onSelect , count : number}) {
+
+    function handleClick() {
+        if (props.children === "+") {
+            props.onSelect(props.count + 1)
+        } else if (props.children === "-") {
+            props.onSelect(props.count - 1)
+        }
+    }
+
+
     return (
-        <button onClick={() => props.onSelect(props.children)}>
-            {props.children}
-        </button>
-    );
+        <>
+            <button onClick={() =>{handleClick()}}>{props.children}</button>
+        </>
+    )
+
 }
