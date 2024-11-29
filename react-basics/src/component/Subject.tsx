e82d27e6b5f60ca5ddbef2fcb3870359b6be1ef3
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-29 12:36:39 +0530

    modify Subject.tsx

--- react-basics/src/component/Subject.tsx
+++ react-basics/src/component/Subject.tsx
@@ -1,8 +1,10 @@
-export function Subject(props : {name : string, children : string}) {
+import {ReactNode} from "react";
+
+export function Subject({name, children} : {name : string, children : ReactNode} ) {
     return (
         <>
-            <h1>{props.name}</h1>
-            <p>{props.children}</p>
+            <h2>{name}</h2>
+            <p>{children}</p>
         </>
     );
 }
