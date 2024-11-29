010df45a7a10ea1ea43713737eb6a0f1080b4c0d
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-29 10:12:41 +0530

    import multiple components in react

--- react-basics/src/App.tsx
+++ react-basics/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
-import ButtonComponent from "./component/ButtonComponent.tsx";
+import {ButtonComponent, SmallButtonComponent} from "./component/ButtonComponent.tsx"; // not recommend : breaking the rule of solid principle which is single responsibility principle
 
 function App() {
 
   return (
+    <>
     <ButtonComponent/>
+    <SmallButtonComponent/>
+    </>
   );
 }
 
--- react-basics/src/component/ButtonComponent.tsx
+++ react-basics/src/component/ButtonComponent.tsx
@@ -1,9 +1,19 @@
-export default function ButtonComponent() {
+export function ButtonComponent() {
     return (
         <>
             <button> click me</button>
             <button> click me</button>
             <button> click me</button>
+        </>
+    );
+}
+
+export function SmallButtonComponent() {
+    return (
+        <>
+            <button>lil click me</button>
+            <button>lil click me</button>
+            <button>lil click me</button>
         </>
     );
 }
