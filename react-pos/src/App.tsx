018eb3b2f09b240b72d71be1ebe13b4cf5f2f4fe
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-12-13 09:46:18 +0530

    input handling with react usestate

--- react-pos/src/App.tsx
+++ react-pos/src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css'
+import {ChangeEvent, useState} from "react";
+
 
 function App() {
-  return (
-    <>
 
-    </>
+  const [names, setNames] = useState({
+    firstName: 'John',
+    lastName: 'Doe'
+  })
+
+  function handleNameChange(event: ChangeEvent<HTMLInputElement>){
+    setNames({
+      ...names,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  return (
+      <>
+        <input name='firstName' type='text' placeholder='First Name' onChange={handleNameChange}></input>
+        <input name='lastName' type='text' placeholder='Last Name' onChange={handleNameChange}></input>
+
+        <br/>
+
+        <h2>{names.firstName} {names.lastName}</h2>
+      </>
   )
 }
 
