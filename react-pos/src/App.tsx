3da7ae6c6e1e51649f4707f41967a84301cce537
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-12-13 10:12:42 +0530

    refactor name handling to use separate state for first and last names

--- react-pos/src/App.tsx
+++ react-pos/src/App.tsx
@@ -1,31 +1,35 @@
 import './App.css'
-import {ChangeEvent, useState} from "react";
-
+import {useState} from "react";
 
 function App() {
 
-  const [names, setNames] = useState({
-    firstName: 'John',
-    lastName: 'Doe'
-  })
-
-  function handleNameChange(event: ChangeEvent<HTMLInputElement>){
-    setNames({
-      ...names,
-      [event.target.name]: event.target.value
+    const [names, setNames] = useState({
+        firstName: 'John',
+        lastName: 'Doe'
     })
-  }
 
-  return (
-      <>
-        <input name='firstName' type='text' placeholder='First Name' onChange={handleNameChange}></input>
-        <input name='lastName' type='text' placeholder='Last Name' onChange={handleNameChange}></input>
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
 
-        <br/>
+    function handleNameChange(){
+        setNames({
+            ...names,
+            firstName: firstName,
+            lastName: lastName
+        })
+    }
 
-        <h2>{names.firstName} {names.lastName}</h2>
-      </>
-  )
+    return (
+        <>
+            <input name='firstName' type='text' placeholder='First Name' onChange={(e) => {setFirstName(e.target.value)}}></input>
+            <input name='lastName' type='text' placeholder='Last Name' onChange={(e) => {setLastName(e.target.value)}}></input>
+            <button onClick={handleNameChange}> Display</button>
+
+            <br/>
+
+            <h2>{names.firstName} {names.lastName}</h2>
+        </>
+    )
 }
 
 export default App
