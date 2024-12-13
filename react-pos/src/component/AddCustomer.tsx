d17ea1f46af527df338bc38087c5dc71910c41ea
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-12-13 13:56:32 +0530

    react navigation links

--- react-pos/src/component/AddCustomer.tsx
+++ react-pos/src/component/AddCustomer.tsx
@@ -1,7 +1,10 @@
+import {Link} from "react-router-dom";
+
 export function AddCustomer() {
     return (
         <>
             <h3>Add Customer</h3>
+            <Link to="/">Go Back </Link>
         </>
     );
 }
--- react-pos/src/component/Dashboard.tsx
+++ react-pos/src/component/Dashboard.tsx
@@ -1,7 +1,10 @@
+import {Link} from "react-router-dom";
+
 export function Dashboard() {
     return (
         <>
             <h3>Dashboard</h3>
+            <Link to="/add">Add Customer</Link>
         </>
     );
 }
