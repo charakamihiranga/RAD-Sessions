89f57c90c898ecafa4fe7ac9e4ee80c335a143b3
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-12-18 12:34:32 +0530

    add customer management functionality with react reducer

--- react-reducer/src/App.tsx
+++ react-reducer/src/App.tsx
@@ -1,22 +1,45 @@
 import { useReducer, useState } from 'react';
 import './App.css';
-import { nameReducer } from './reducers/NameReducer.ts';
+import { CustomerReducer } from './reducers/CustomerReducer.ts';
+import { Customer } from './model/Customer.ts';
 
-const initialState = { firstName: '', lastName: '' };
+const initialCustomerState: Customer[] = [];
 
 function App() {
-    const [name, dispatch] = useReducer(nameReducer, initialState);
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [customer, dispatch] = useReducer(CustomerReducer, initialCustomerState);
+
+    const [name, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    function addCustomer() {
+        const newCustomer = new Customer(name, email, phone);
+        dispatch({ type: "ADD_CUSTOMER", payload: newCustomer });
+    }
 
     return (
         <>
-            <h1>{name.firstName} {name.lastName}</h1>
-            <br />
-            <input type="text" onChange={(e) => setFirstName(e.currentTarget.value)} />
-            <input type="text" onChange={(e) => setLastName(e.currentTarget.value)} />
-            <br />
-            <button onClick={() => dispatch({ type: "PUSH_NAME", payload: { firstName, lastName } })}>Show</button>
+            <header><h2>Customer Management</h2></header>
+            <br/>
+
+            <input type="text" placeholder="name" value={name} onChange={(e) => { setFirstName(e.target.value) }} />
+            <input type="text" placeholder="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+            <input type="text" placeholder="phone" value={phone} onChange={(e) => { setPhone(e.target.value) }} />
+
+            <br/>
+            <button onClick={addCustomer}>Add Customer</button>
+
+            <br/>
+            <br/>
+
+            <h3>Customers</h3>
+            <br/>
+
+            {customer.map((customer: Customer, index: number) => (
+                <div key={index}>
+                    {customer.name} - {customer.email}
+                </div>
+            ))}
         </>
     );
 }
