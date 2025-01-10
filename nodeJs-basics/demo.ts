c9ac59345a81d2a4d765d301118d1e193dcf8368
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2025-01-10 16:07:30 +0530

    form handling and body-parser middleware to Node.js server

--- nodeJs-basics/demo.ts
+++ nodeJs-basics/demo.ts
@@ -1,12 +1,22 @@
+import bodyParser from "body-parser";
 import express from "express";
 
-const server = express()
+const app = express();
 
-server.get('/', (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/send", (req, res, next) => {
   console.log(req);
-  res.send('<h1>Express works...</h1>');
-})
+  res.send(
+    '<html><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>'
+  );
+});
 
-server.listen(3000, () => {
+app.post("/message", (req, res, next) => {
+  console.log(req.body.message);
+  res.send("<h4>Message Endpoint Called</h4>");
+});
+
+app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
