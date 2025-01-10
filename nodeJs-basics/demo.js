2f18210c6979535f7b5c5f2a7b0583d485819a70
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2025-01-10 12:17:50 +0530

    add file writing functionality and improve request handling in Node.js server

--- nodeJs-basics/demo.js
+++ nodeJs-basics/demo.js
@@ -1,13 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
+var fs = require("fs");
 var server = http.createServer(function (req, res) {
-    if (req.url === '/') {
-        res.write('<h1>Hello World</h1>');
+    if (req.url === "/add") {
+        fs.writeFile("demo.txt", req.url, function (err) {
+            if (err) {
+                res.write("<h1>Error: File not created</h1>");
+            }
+        });
+        res.write("<h1>Hello Customer</h1>");
+        return res.end();
     }
-    else if (req.url === '/add') {
-        res.write('<h1>Hello Customer</h1>');
-    }
+    res.write("<h1>Hello World</h1>");
     res.end();
+    console.log("Request received", req);
+    process.exit();
 });
-server.listen(3000);
+server.listen(3000, function () {
+    console.log("Server is listening on port 3000");
+});
--- nodeJs-basics/demo.ts
+++ nodeJs-basics/demo.ts
@@ -1,13 +1,22 @@
-import * as http from 'http';
+import * as http from "http";
+import * as fs from "fs";
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.write('<h1>Hello World</h1>');
-       
-    } else if(req.url === '/add'){
-        res.write('<h1>Hello Customer</h1>');
-    }
-    res.end();
-})
+  if (req.url === "/add") {
+    fs.writeFile("demo.txt", req.url, (err) => {
+        if (err) {
+            res.write("<h1>Error: File not created</h1>");
+        }
+    });
+    res.write("<h1>Hello Customer</h1>");
+    return res.end();
+  }
+  res.write("<h1>Hello World</h1>");
+  res.end();
+  console.log("Request received", req);
+  process.exit();
+});
 
-server.listen(3000)
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
