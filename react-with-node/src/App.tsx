1c68baf0abf5eec541cb7e54052c3b5ee4728550
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2025-01-24 09:27:39 +0530

    react useEffect example

---
+++ react-with-node/src/App.tsx
@@ -0,0 +1,21 @@
+import {useEffect, useState} from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+    useEffect(() => {
+        console.log("useEffect triggered:::");
+    }, [count, count2]);
+  return (
+    <>
+    <button onClick={() => setCount(count+1)}>Count:: {count}</button>
+      <br/>
+      <br/>
+    <button onClick={() => setCount2(count2+1)}>Count:: {count2}</button>
+    </>
+  )
+}
+
+export default App
